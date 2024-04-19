f7dc22c79db343dcc197df224038f7335764eb76
Mam0rr <141276887+Mam0rr@users.noreply.github.com>
2024-04-19 22:49:19 +0100

    Update PacketSniffer.cs

--- PacketSniffer.cs
+++ PacketSniffer.cs
@@ -1,5 +1,9 @@
 using PacketDotNet;
 using SharpPcap;
+using SharpPcap.LibPcap;
+using System.IO;
+using System.Net;
+using static System.Net.WebRequestMethods;
 
 class PacketSniffer
 {
@@ -24,6 +28,49 @@ class PacketSniffer
         }
     }
 
+    private static string setFilters(string input)
+    {
+        switch (input)
+        {
+            case "tcp":
+                {
+                    return "tcp";
+                }
+            case "udp":
+                {
+                    return "udp";
+                }
+            case "icmp":
+                {
+                    return "icmp";
+                }
+            default:
+                {
+                    Console.WriteLine("Unknown input");
+                    return null;
+                }
+        }
+
+    }
+
+    private static string setFilters(string input, string input2)
+    {
+        switch (input.ToLower())
+        {
+            case "srcip":
+                return $"src host {input2.Trim()}";
+            case "dstip":
+                return $"dst host {input2.Trim()}";
+            case "srcport":
+                return $"src port {input2.Trim()}";
+            case "dstport":
+                return $"dst port {input2.Trim()}";
+            default:
+                Console.WriteLine("Unknown input");
+                return null;
+        }
+    }
+
     static void Main(string[] args)
     {
         bool exiting = false;
@@ -40,79 +87,75 @@ class PacketSniffer
             string[] parts = cmd.Split(' ');
 
 
-            switch (parts[0])
+            switch (parts[0].Trim().ToLower())
             {
                 case "run":
                     {
-                        if (parts.Length == 1)
+                        Console.WriteLine("Scanning for available devices...\n");
+
+                        var devices = CaptureDeviceList.Instance;
+
+                        if (devices.Count < 1)
                         {
-                            Console.WriteLine("Scanning for available devices...\n");
-
-                            var devices = CaptureDeviceList.Instance;
-
-                            if (devices.Count < 1)
-                            {
-                                Console.WriteLine("No devices were found on this machine");
-                                return;
-                            }
-
-                            Console.WriteLine("\nThe following devices are available on this machine:");
-                            Console.WriteLine("----------------------------------------------------\n");
-
-                            int i = 0;
-
-                            foreach (var dev in devices)
-                            {
-                                Console.WriteLine("{0} Name: {1}   MAC: {2}    Description: {3}", i, dev.Name, dev.MacAddress, dev.Description);
-                                i++;
-                            }
-
-                            Console.WriteLine();
-                            Console.Write("-- Please choose a device to capture: ");
-                            i = int.Parse(Console.ReadLine());
-
-                            using var device = devices[i];
-
-                            device.OnPacketArrival +=
-                                new PacketArrivalEventHandler(device_OnPacketArrival);
-
-                            int readTimeoutMilliseconds = 1000;
-                            device.Open(mode: DeviceModes.Promiscuous | DeviceModes.DataTransferUdp | DeviceModes.NoCaptureLocal, read_timeout: readTimeoutMilliseconds);
-
-                            Console.WriteLine("\n-- Listening on {0} {1}, hit 'Enter' to stop...",
-                                device.Name, device.Description);
-
-                            Console.WriteLine("------------------------------------------------------------------------------------------------------");
-
-
-                            device.StartCapture();
-
-                            Console.ReadLine();
-
-                            device.StopCapture();
-
-                            Console.WriteLine("-- Capture stopped.");
-
-                            Console.WriteLine($"-- Packets Received: {device.Statistics.ReceivedPackets}, Dopped Packets: {device.Statistics.DroppedPackets}, Interface Dropped Packets: {device.Statistics.InterfaceDroppedPackets}");
-
-
-                            break;
-                        }
-                        else if (parts.Length == 2)
-                        {
-
-                            break;
-                        }
-                        else if (parts.Length == 3)
-                        {
-
-                            break;
-                        }
-                        else
-                        {
-                            break;
+                            Console.WriteLine("No devices were found on this machine");
+                            return;
                         }
 
+                        Console.WriteLine("\nThe following devices are available on this machine:");
+                        Console.WriteLine("----------------------------------------------------\n");
+
+                        int i = 0;
+
+                        foreach (var dev in devices)
+                        {
+                            Console.WriteLine("{0} Name: {1}   MAC: {2}    Description: {3}", i, dev.Name, dev.MacAddress, dev.Description);
+                            i++;
+                        }
+
+                        Console.WriteLine();
+                        Console.Write("-- Please choose a device to capture: ");
+                        i = int.Parse(Console.ReadLine());
+
+                        using var device = devices[i];
+
+                        device.OnPacketArrival +=
+                            new PacketArrivalEventHandler(device_OnPacketArrival);
+
+                        int readTimeoutMilliseconds = 1000;
+                        device.Open(mode: DeviceModes.Promiscuous | DeviceModes.DataTransferUdp | DeviceModes.NoCaptureLocal, read_timeout: readTimeoutMilliseconds);
+
+                        string filter = null;
+
+                        if (parts.Length == 2)
+                        {
+                            filter = setFilters(parts[1].Trim().ToLower());
+                        }
+                        if (parts.Length == 3)
+                        {
+                            filter = setFilters(parts[1].Trim().ToLower(), parts[2].Trim().ToLower());
+                        }
+
+                        device.Filter = filter;
+
+
+
+                        Console.WriteLine("\n-- Listening on {0} {1}, hit 'Enter' to stop...",
+                            device.Name, device.Description);
+
+                        Console.WriteLine("------------------------------------------------------------------------------------------------------");
+
+
+                        device.StartCapture();
+
+                        Console.ReadLine();
+
+                        device.StopCapture();
+
+                        Console.WriteLine("-- Capture stopped.");
+
+                        Console.WriteLine($"-- Packets Received: {device.Statistics.ReceivedPackets}, Dopped Packets: {device.Statistics.DroppedPackets}, Interface Dropped Packets: {device.Statistics.InterfaceDroppedPackets}");
+
+                        break;
                     }
                 case "exit":
                     {
